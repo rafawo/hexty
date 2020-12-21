52893d6adc2cb42d26c91903481a87e284062475
Rafael Alcaraz Mercado <rafawo1@hotmail.com>
2020-12-21 15:38:18 -0800

    Add some more properties

--- hexty/Assets/src/physhex/PhysHex.cs
+++ hexty/Assets/src/physhex/PhysHex.cs
@@ -22,6 +22,30 @@ public class Particle
     /// Holds the acceleration of the particle in world space.
     /// </summary>
     public Vector2 Acceleration;
+
+    /// <summary>
+    /// Holds the amount of damping applied to linear motion.
+    /// Damping is required to remove energy added through numerical
+    /// instability in the integrator.
+    /// </summary>
+    public float Damping;
+
+    /// <summary>
+    /// Holds the inverse of the mass of the particle.
+    /// It is more useful to hold the inverse mass because
+    /// integration is simpler and because in real-time simulation
+    /// it is more useful to have objects with infinite mass (immovable)
+    /// than zero mass (completely unstable in numerical simulation).
+    /// </summary>
+    public float InverseMass;
+
+    /// <summary>
+    /// Sets the InverseMass property based on the mass supplied in this function.
+    /// </summary>
+    public void SetMass(float mass)
+    {
+        InverseMass = 1 / mass;
+    }
 }
 
 }
