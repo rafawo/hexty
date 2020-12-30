63c725d252db240ce8dcc995182a14b1b7d92f87
Rafael Alcaraz Mercado <rafawo1@hotmail.com>
2020-12-30 13:51:36 -0800

    Group all relevant input thematically by Horizontal and Vertical movement

--- hexty/Assets/src/HexGrid.cs
+++ hexty/Assets/src/HexGrid.cs
@@ -373,16 +373,66 @@ public class HexGrid : MonoBehaviour
         }
     }
 
-    private enum InputDirection { Left, Right, Up, Down }
+    private enum InputDirection { Left, Right, Down, Up }
 
+    /// <summary>
+    /// Returns whether the specified input direction is active.
+    ///
+    /// This function assumes that the input manager has setup
+    /// left-right movement with "Horizontal" and
+    /// down-up movement with "Vertical".
+    ///
+    /// -- Ideal Horizontal Input Setup --
+    ///
+    /// Left: [
+    ///     A,
+    ///     Left arrow,
+    ///     Controller X axis (left joystick) < 0,
+    ///     Controller 4th X axis (right joystick) < 0,
+    ///     Controller 6th X axis (dpad) < 0,
+    /// ]
+    /// Right: [
+    ///     D,
+    ///     Right arrow,
+    ///     Controller X axis (left joystick) > 0,
+    ///     Controller 4th X axis (right joystick) > 0,
+    ///     Controller 6th X axis (dpad) > 0,
+    /// ]
+    ///
+    /// -- Ideal Vertical Input Setup --
+    ///
+    /// Down: [
+    ///     S,
+    ///     Down arrow,
+    ///     Controller Y axis (left joystick) < 0,
+    ///     Controller 5th Y axis (right joystick) < 0,
+    ///     Controller 7th Y axis (dpad) < 0,
+    /// ]
+    /// Up: [
+    ///     W,
+    ///     Up arrow,
+    ///     Controller inverted Y axis (left joystick) > 0,
+    ///     Controller inverted 5th Y axis (right joystick) > 0,
+    ///     Controller inverted 7th Y axis (dpad) > 0,
+    /// ]
+    /// </summary>
+    /// <param name="direction">Input direction to be queried if active.</param>
+    /// <returns></returns>
     private bool IsDirectionActive(InputDirection direction)
     {
         switch (direction)
         {
-            case InputDirection.Left: return Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A) || Input.GetAxis("Horizontal") < 0;
-            case InputDirection.Right: return Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D) || Input.GetAxis("Horizontal") > 0;
-            case InputDirection.Down: return Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S) || Input.GetAxis("Vertical") < 0;
-            case InputDirection.Up: return Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W) || Input.GetAxis("Vertical") > 0;
+            case InputDirection.Left:
+                return Input.GetAxis("Horizontal") < 0;
+
+            case InputDirection.Right:
+                return Input.GetAxis("Horizontal") > 0;
+
+            case InputDirection.Down:
+                return Input.GetAxis("Vertical") < 0;
+
+            case InputDirection.Up:
+                return Input.GetAxis("Vertical") > 0;
         }
         return false;
     }
